a82ddd4b3d57d708b0c55b6324678440f40d72b7
matsuda-kazuhide <e185756@ie.u-ryukyu.ac.jp>
2020-10-27 17:06:42 +0900

    regression.py fit

--- main.py
+++ main.py
@@ -8,3 +8,9 @@ print(Y)
 import regression
 model = regression.LinearRegression()
 print(model.x)
+
+import importlib
+importlib.reload(regression)
+model = regression.LinearRegression()
+model.fit(X,Y)
+print(model.theta)
