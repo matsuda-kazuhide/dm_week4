acf84239b8b2980e0d6975f3e580f4abe50c9a76
matsuda-kazuhide <e185756@ie.u-ryukyu.ac.jp>
2020-11-10 17:39:33 +0900

    3次元 グラフ

--- datasets.py
+++ datasets.py
@@ -3,7 +3,7 @@ def load_nonlinear_example1():
     X = np.array([[1,0.0],[1,2.0],[1,3.9],[1,4.0]])
     Y = np.array([4.0,0.0,3.0,2.0])
     return X,Y
-def polynomial2_features(input):
+def polynomial3_features(input):
     poly2 = input[:,1:]**2
     poly3 = input[:,1:]**3
     return np.c_[input, poly2, poly3]
--- nonlinear_ex.py
+++ nonlinear_ex.py
@@ -3,13 +3,13 @@ import datasets
 import regression
 
 X,Y = datasets.load_nonlinear_example1()
-ex_X = datasets.polynomial2_features(X)
+ex_X = datasets.polynomial3_features(X)
 model = regression.LinearRegression()
 model.fit(ex_X,Y)
 
 samples = np.arange(0, 4, 0.1)
 x_samples = np.c_[np.ones(len(samples)),samples]
-ex_x_samples = datasets.polynomial2_features(x_samples)
+ex_x_samples = datasets.polynomial3_features(x_samples)
 
 import matplotlib.pyplot as plt
 plt.scatter(X[:,1],Y)
